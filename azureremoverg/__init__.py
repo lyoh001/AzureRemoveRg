1ffbec718f2870d3a73d5064380abaa4acc6dea8
lyoh001@hotmail.com <lyoh001@hotmail.com>
2021-02-10 23:58:29 +1100

    Commit

--- azureremoverg/__init__.py
+++ azureremoverg/__init__.py
@@ -6,41 +6,36 @@ import requests
 
 
 def get_rest_api_token():
+    oauth2_headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    oauth2_body = {
+        "client_id": os.environ["REST_CLIENT_ID"],
+        "client_secret": os.environ["REST_CLIENT_SECRET"],
+        "grant_type": "client_credentials",
+        "resource": "https://management.azure.com",
+    }
+    oauth2_url = (
+        f"https://login.microsoftonline.com/{os.environ['TENANT_ID']}/oauth2/token"
+    )
     try:
-        oauth2_headers = {"Content-Type": "application/x-www-form-urlencoded"}
-        oauth2_body = {
-            "client_id": os.environ["REST_CLIENT_ID"],
-            "client_secret": os.environ["REST_CLIENT_SECRET"],
-            "grant_type": "client_credentials",
-            "resource": "https://management.azure.com",
-        }
-        oauth2_url = (
-            f"https://login.microsoftonline.com/{os.environ['TENANT_ID']}/oauth2/token"
-        )
         return requests.post(
             url=oauth2_url, headers=oauth2_headers, data=oauth2_body
         ).json()["access_token"]
 
     except Exception as e:
-        logging.info(str(e))
+        logging.info(f"{e}")
 
 
 def get_rest_api_headers(token):
-    try:
-        rest_api_headers = {
-            "Authorization": "Bearer " + token,
-            "Content-Type": "application/json",
-        }
-        return rest_api_headers
-
-    except Exception as e:
-        logging.info(str(e))
+    return {
+        "Authorization": "Bearer " + token,
+        "Content-Type": "application/json",
+    }
 
 
 def main(mytimer: func.TimerRequest) -> None:
     logging.info("******* Starting the function *******")
+    rest_api_headers = get_rest_api_headers(get_rest_api_token())
     try:
-        rest_api_headers = get_rest_api_headers(get_rest_api_token())
         logging.info(
             [
                 requests.delete(
